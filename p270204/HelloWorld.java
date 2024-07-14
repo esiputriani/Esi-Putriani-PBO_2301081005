49ab4a6ce2f889135f7183b6139e2f66f0b23ac3
esiputriani <167851090+esiputriani@users.noreply.github.com>
2024-07-14 10:12:12 +0700

    Add files via upload

---
+++ p270204/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pbo2301081005.p270204;
+
+/**
+ *
+ * @author Administrator
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+    
+}
